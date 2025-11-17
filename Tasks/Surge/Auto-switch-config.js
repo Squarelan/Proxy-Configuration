bdd654f11d1f49844fa03721f25cf03f5e871e06
Squarelan <46771456+Squarelan@users.noreply.github.com>
2025-11-17 17:33:48 +0800

    Create Auto-switch-config.js

---
+++ Tasks/Surge/Auto-switch-config.js
@@ -0,0 +1,28 @@
+//Surge 自动切换配置脚本
+
+//[Script]
+//script1 = type=event,event-name=network-changed,script-path=wifi-changed.js
+
+const WIFI_DONT_NEED_PROXYS = ['钵钵鸡-5G'];
+const CURRENT_WIFI_SSID_KEY = 'current_wifi_ssid';
+
+if (wifiChanged()) {
+  const mode = WIFI_DONT_NEED_PROXYS.includes($network.wifi.ssid)
+    ? 'direct'
+    : 'rule';
+  $surge.setOutboundMode(mode);
+  $notification.post(
+    'Surge',
+    `Wi-Fi changed to ${$network.wifi.ssid || 'cellular'}`,
+    `use ${mode} mode`
+  );
+}
+
+function wifiChanged() {
+  const currentWifiSSid = $persistentStore.read(CURRENT_WIFI_SSID_KEY);
+  const changed = currentWifiSSid !== $network.wifi.ssid;
+  changed && $persistentStore.write($network.wifi.ssid, CURRENT_WIFI_SSID_KEY);
+  return changed;
+}
+
+$done();
